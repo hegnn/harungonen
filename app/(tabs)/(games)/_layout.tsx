3da73968e2138305a8e6c30b2138c38350b5cafe
Harun Gönen <hegonen98@gmail.com>
2024-07-30 00:07:36 +0300

    fix: initial route name changed with "index" instead of "games"

--- app/(tabs)/(games)/_layout.tsx
+++ app/(tabs)/(games)/_layout.tsx
@@ -2,8 +2,8 @@ import { Stack } from 'expo-router';
 
 export default function GamesLayout() {
   return (
-    <Stack  initialRouteName='games'>
-      <Stack.Screen name='games' options={{title: "Game List"}} />
+    <Stack>
+      <Stack.Screen name='index' options={{title: "Game List"}} />
       <Stack.Screen name='details' options={{title: "Details"}}/>
     </Stack>
   );
